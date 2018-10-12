9c1b86182645417f21975bda42cb4c5d480f7f91
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-12 23:40:23 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(veggies)
   result
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  calls_long.any?{|word|word.count > 4}
 end
 
 def find_the_cheese# code an argument here

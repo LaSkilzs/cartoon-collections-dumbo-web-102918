6ee3ceb0c1e5c0a54be48518978345147a26b1ef
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-12 23:45:26 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(calls)
   calls.any? {|word|word.size > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.include?(cheese)
 end
